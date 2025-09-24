cecad63d1c4fca0dc6e82ae064f78c83e9c283de
corsal2006 <siddeshnaik292006@gmail.com>
2025-09-25 00:35:52 +0530

    Add files via upload

--- TranslatorApp/build.sh
+++ TranslatorApp/build.sh
@@ -2,8 +2,9 @@
 # exit on error
 set -o errexit
 
-# NEW: Upgrade pip and build tools first
-pip install --upgrade pip setuptools wheel
+# FIX: Forcefully upgrade pip and install a specific, stable version of setuptools
+pip install --upgrade pip
+pip install "setuptools==65.5.0"
 
 # Install system dependencies
 apt-get update && apt-get install -y tesseract-ocr tesseract-ocr-nep tesseract-ocr-sin poppler-utils
